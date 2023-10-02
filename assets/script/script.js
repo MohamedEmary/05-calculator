70e0a6fc74948c73865ad48b23c42cee64148ca7
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2023-10-02 17:30:15 +0300

    Implement calculator UI functionality
    - Add event listeners for button clicks
    - Store inputs and operator into variables
    - Write inputs to lower display
    - Clear displays on operator input
    - Calculate result on equal button
    - Display result in upper display
    - Reset calculator state

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,23 +14,126 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
+const buttons = document.querySelectorAll(".key-row button");
+buttons.forEach((btn) =>
+  btn.addEventListener("click", () => handleButtonClick(btn))
+);
+
+const upperDisplay = document.querySelector("#upper-display");
+const lowerDisplay = document.querySelector("#lower-display");
+const MAX_LENGTH = 12;
+let num1 = "";
+let num2 = "";
+let operator = "";
+let num1Assign = true;
+let num2Assign = false;
+
+function writeToScreen() {
+  return lowerDisplay.textContent.length < MAX_LENGTH;
+}
+
+function assignNum() {
+  if (num1Assign) {
+    num1 = Number(lowerDisplay.textContent);
+  } else {
+    num2 = Number(lowerDisplay.textContent);
+  }
+}
+
+
+function handleDigits(btn) {
+  if (
+    !(btn.value === "0" && lowerDisplay.textContent === "") && // to prevent writing 0 as the first digit
+    writeToScreen()
+  ) {
+    // to clear the display after writing the operator
+    if (num2Assign && num2 === "") {
+      lowerDisplay.textContent = "";
+    }
+
+    lowerDisplay.textContent += btn.value;
+    assignNum();
+  }
+}
+
+function handleDot(btn) {
+  // !lowerDisplay.textContent.includes(".") to prevent multiple dots
+  if (!lowerDisplay.textContent.includes(".") && writeToScreen()) {
+    lowerDisplay.textContent += btn.value;
+  }
+  // no need to reassign num1 or num2 here as dot is useless unless there is a number after it
+  // and when there is a number after it, it will be reassigned in handleDigits() function using
+  // assignNum() function
+}
+
+function handleOperators(btn) {
+  if (
+    // activate only once
+    num1Assign &&
+    // activate if num1 is assigned
+    num1 !== "" &&
+    //don't activate if there is no number
+    !(num2 === "" && num1 === "")
+  ) {
+    lowerDisplay.textContent = btn.value;
+    operator = btn.value;
+    num1Assign = false;
+    num2Assign = true;
+  }
+}
+
+function reset() {
+  let num1 = "";
+  let num2 = "";
+  let operator = "";
+  let num1Assign = true;
+  let num2Assign = false;
+}
+function handleEqual() {
+  let result = 0;
+  if (num1 !== "" && num2 !== "") {
+    result = operate(num1, num2, operator);
+  }
+  lowerDisplay.textContent = "";
+  upperDisplay.textContent = result;
+  reset();
+}
+
+function handleButtonClick(btn) {
+  let btnClass = btn.className;
+  let btnId = btn.id;
+  if (btnClass === "digit") {
+    // btnClass is used for multiple buttons with the same class
+    handleDigits(btn);
+  } else if (btnId === "dot") {
+    // btnId is used for one button with that unique id
+    handleDot(btn);
+  } else if (btnClass === "operator") {
+    handleOperators(btn);
+  } else if (btnId === "equal") {
+    handleEqual();
+  }
+}
+
 function operate(num1, num2, operator) {
+  let result = 0;
   switch (operator) {
     case "+":
-      add(num1, num2);
+      result = add(num1, num2);
       break;
     case "-":
-      subtract(num1, num2);
+      result = subtract(num1, num2);
       break;
     case "*":
-      multiply(num1, num2);
+      result = multiply(num1, num2);
       break;
     case "/":
-      divide(num1, num2);
+      result = divide(num1, num2);
       break;
 
     // handle default behavior later
     default:
       break;
   }
+  return result;
 }
